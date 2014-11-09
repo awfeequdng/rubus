eb27514567ef6f72ab394d64a3dd60eb8af0f7b2
Wulff007 <Wulff007@gmail.com>
2014-11-09 12:05:02 +0300

    demo_data.sql - add gui settings

--- sql/demo_data.sql
+++ sql/demo_data.sql
@@ -9,3 +9,24 @@ INSERT INTO items (it_name, it_article, it_unit, it_type) VALUES
 ('Chicken egg', '', 'EA', 'M'),
 ('Chicken body', '', 'KG', 'M'),
 ('Chicken feet', '', 'KG', 'M');
+
+INSERT INTO user_params (up_role, up_name, up_gui) VALUES ('rubus', 'Rubus admin', 
+'<gui>
+	<widgets>
+	</widgets>
+	<mainmenu>
+		<menu id="" title="General">
+      		<action id="Base.UserManager">Users</action>
+      		<action id="Base.ReportManager">Reports manager</action>
+      		<separator />
+			<action id="Core.CloseSession" />
+			<separator />
+			<action id="MainWindow.Close" />
+		</menu>
+		<menu id="" title="Catalogs">
+			<action id="Base.Locations">Locations</action>
+			<action id="Base.Contractors">Contractors</action>
+ 			<action id="Base.Items">Items</action>
+		</menu>
+	</mainmenu>
+</gui>');

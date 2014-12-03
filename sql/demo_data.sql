1f200506328bf0fc8b15874cab754ef9405daf41
wulff007 <wulff007@gmail.com>
2014-12-03 15:39:46 +0300

    fix demo_data.sql

--- sql/demo_data.sql
+++ sql/demo_data.sql
@@ -1,3 +1,6 @@
+--Uncomment this if can create new role of PostgreSQL
+--CREATE ROLE rubus WITH SUPERUSER LOGIN PASSWORD 'rubus';
+
 INSERT INTO units (un_id, un_name,un_desc) VALUES
 ('EA','ea.', 'each'),
 ('KG','kg.', 'kilogram');
@@ -10,6 +13,9 @@ INSERT INTO items (it_name, it_article, it_unit, it_type) VALUES
 ('Chicken body', '', 'KG', 'M'),
 ('Chicken feet', '', 'KG', 'M');
 
+INSERT INTO contractors (co_name, co_type) VALUES ('bzrk', 2), ('bsrk 2', 2), ('Mr. Putin', 1);
+
+-- Change rolename for params
 INSERT INTO user_params (up_role, up_name, up_gui) VALUES ('rubus', 'Rubus admin', 
 '<gui>
 	<widgets>
@@ -17,7 +23,7 @@ INSERT INTO user_params (up_role, up_name, up_gui) VALUES ('rubus', 'Rubus admin
 	<mainmenu>
 		<menu id="" title="General">
       		<action id="Base.UserManager">Users</action>
-      		<action id="Base.ReportManager">Reports manager</action>
+      		<action id="Reports.ReportManager">Reports manager</action>
       		<separator />
 			<action id="Core.CloseSession" />
 			<separator />
